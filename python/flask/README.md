ea5463bad7bb2722d6de8079877285e180d6409d
Marcus Wyche <mwyche@phillies.com>
2019-11-15 11:53:17 -0500

    Updates to docker file and readme for installing uwsgi

--- python/flask/README.md
+++ python/flask/README.md
@@ -34,3 +34,14 @@ docker run -it datadidit/uwsgi
 
 To verify websockets are working go to [http://localhost:8080](http://localhost:8080) you will
 see network connections to the socketio endpoint were successful.
+
+## Troubleshooting
+
+* IOError: unable to complete websocket handshake while running in uwsgi?
+
+This is happening because `uwsgi` was not installed with [ssl](https://stackoverflow.com/a/38842664) support. To ensure it's installed with ssl support
+run this command:
+
+```bash
+CFLAGS="-I/usr/local/opt/openssl/include" LDFLAGS="-L/usr/local/opt/openssl/lib" UWSGI_PROFILE_OVERRIDE=ssl=true pip install uwsgi -Iv --no-cache-dir
+```
--- python/flask/app.py
+++ python/flask/app.py
@@ -1,5 +1,6 @@
+import logging
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -19,7 +20,27 @@ def hello(name="World"):
 
 @socketio.on('message')
 def handle_message(message):
-    print('received message: ' + message)
+    logging.info('received message: ' + message)
+
+
+@socketio.on('my_echo', namespace='/draft')
+def test_message(message):
+    logging.info('GOT my_echo: %s' % message)
+    emit('my_response',
+         {'data': message['data']})
+
+
+@socketio.on('my_broadcast_event', namespace='/draft')
+def test_broadcast_message(message):
+    logging.info('GOT my_broadcast')
+    emit('my_response',
+         {'data': message['data']},
+         broadcast=True)
+
+
+@socketio.on('my_ping', namespace='/draft')
+def ping_pong():
+    emit('my_pong')
 
 
 if __name__ == "__main__":

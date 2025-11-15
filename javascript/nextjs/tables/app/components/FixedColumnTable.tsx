57c37862811b93917765f1fe2d3f642742443387
Marcus Wyche <mkwyche@gmail.com>
2025-11-15 16:45:01 -0500

    Basic table with sorting working

---
+++ javascript/nextjs/tables/app/components/FixedColumnTable.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { useState } from "react";
+import { ChevronDown, ChevronUp} from "lucide-react";
+
+const FixedColumnTable = ({columns, data}) => {
+
+    return (
+        <div>
+            <header className="p-5 text-center">
+                <h1>Fixed Column Table</h1>
+            </header>
+        </div>
+    )
+}
+
+export default FixedColumnTable

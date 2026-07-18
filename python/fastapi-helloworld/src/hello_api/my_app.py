d297d8e2fd2f257dcce7dab48f784a3e64649f6e
Marcus Wyche <mkwyche@gmail.com>
2026-07-18 16:19:43 -0400

    Updates to figure out what a file server in swagger would look like

--- python/fastapi-helloworld/src/hello_api/my_app.py
+++ python/fastapi-helloworld/src/hello_api/my_app.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/upload/")
+async def upload_file(file: UploadFile = File(...)):
+    # Read metadata
+    filename = file.filename
+    content_type = file.content_type
+    
+    # Read the actual contents (async)
+    contents = await file.read()
+    
+    return {
+        "filename": filename,
+        "content_type": content_type,
+        "file_size_bytes": len(contents)
+    }

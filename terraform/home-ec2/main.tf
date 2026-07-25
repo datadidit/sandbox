af0c07d8b438c10aff43d26ca4069abfcf8f09c7
Marcus Wyche <mkwyche@gmail.com>
2026-07-25 06:36:49 -0400

    Wrapping this module up

--- terraform/home-ec2/main.tf
+++ terraform/home-ec2/main.tf
@@ -1,29 +1,82 @@
+locals {
+    current_timestamp = timestamp()
+    formatted_timestamp = formatdate("YYYY-MM-DD'T'hh:mmZ", local.current_timestamp)
+}
+
+data "external" "local_user" {
+  program = ["sh", "-c", "echo \"{\\\"user\\\": \\\"$(whoami)\\\"}\""]
+}
+
+data "external" "force_ipv4" {
+  program = ["sh", "-c", "echo \"{\\\"ipv4\\\": \\\"$(curl -4 ifconfig.me)\\\"}\""]
+}
+
 # Automatically query your current local workstation's public IP
 data "http" "my_public_ip" {
   url = "https://ifconfig.me/ip"
 }
 
-# Creates the security group needed for connecting only from your local box
+locals {
+  is_ipv6 = can(regex(":", data.http.my_public_ip.response_body))
+}
+
+# 1. Base Security Group Container (Always created)
 resource "aws_security_group" "ssh_restricted" {
-  name        = "ssh-only-my-ip"
+  name        = "ssh-only-my-ip-test"
   description = "Block all inbound SSH access except for the deployer workspace"
+}
 
-  ingress {
-    description = "SSH from my workstation only"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    # Append the /32 routing suffix dynamically to lock it to your single IP
-    cidr_blocks = ["${chomp(data.http.my_public_ip.response_body)}/32"]
-  }
+# ==========================================
+# IPv4 Rules (Created when local.is_ipv6 = false)
+# ==========================================
 
-  egress {
-    description = "Allow all outbound infrastructure traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# Ingress: SSH via IPv4
+# TODO: figure out how to make this work.
+resource "aws_vpc_security_group_ingress_rule" "ssh_ingress_v4" {
+  # TODO: If you can figure out the ipv6 rule hanging then you can make this conditional
+  # count             = !local.is_ipv6 ? 1 : 0
+  security_group_id = aws_security_group.ssh_restricted.id
+  description       = "SSH from my workstation (IPv4)"
+
+  ip_protocol = "tcp"
+  from_port   = 22
+  to_port     = 22
+  cidr_ipv4   = "${chomp(data.external.force_ipv4.result["ipv4"])}/32"
+}
+
+# Ingress: SSH via IPv6
+# TODO: Below does not work need to figure out proper way to set this up...
+resource "aws_vpc_security_group_ingress_rule" "ssh_ingress_v6" {
+  count             = local.is_ipv6 ? 1 : 0
+  security_group_id = aws_security_group.ssh_restricted.id
+  description       = "SSH from my workstation (IPv6)"
+
+  ip_protocol = "tcp"
+  from_port   = 22
+  to_port     = 22
+  cidr_ipv6   = "${chomp(data.http.my_public_ip.response_body)}/128"
+}
+
+# Egress: All Outbound via IPv4
+resource "aws_vpc_security_group_egress_rule" "all_egress_v4" {
+  security_group_id = aws_security_group.ssh_restricted.id
+  description       = "Allow all outbound IPv4 traffic"
+
+  ip_protocol = "-1"
+  cidr_ipv4   = "0.0.0.0/0"
+}
+
+# ==========================================
+# IPv6 Rules (Created when local.is_ipv6 = true)
+# ==========================================
+
+# Egress: All Outbound via IPv6
+resource "aws_vpc_security_group_egress_rule" "all_egress_v6" {
+  security_group_id = aws_security_group.ssh_restricted.id
+  description       = "Allow all outbound IPv6 traffic"
+
+  ip_protocol = "-1"
+  cidr_ipv6   = "::/0"
 }
 
 # Key pair ssh
@@ -53,4 +106,7 @@ resource "aws_instance" "ec2_instance" {
     vpc_security_group_ids = concat([aws_security_group.ssh_restricted.id], var.aws_additional_security_groups)
     key_name  = data.aws_key_pair.key_pair.key_name
     user_data = var.aws_instance_startup_script
+    tags = {
+        Name = "home-ec2-${local.formatted_timestamp}"
+    }
 }

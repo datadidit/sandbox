a9afa0f275c1b1f1b1e01ebb31132a8e02985359
Marcus Wyche <mkwyche@gmail.com>
2026-07-26 07:38:48 -0400

    Formatting and adding in ability to specify Name and Subnet ID for the ec2 instance

--- terraform/home-ec2/outputs.tf
+++ terraform/home-ec2/outputs.tf
@@ -4,15 +4,15 @@ output "home_ip" {
 }
 
 output "home_sg" {
-    description = "Reference to the home security group"
-    value = {
-        arn = aws_security_group.ssh_restricted.arn
-        id = aws_security_group.ssh_restricted.id
-    }
+  description = "Reference to the home security group"
+  value = {
+    arn = aws_security_group.ssh_restricted.arn
+    id  = aws_security_group.ssh_restricted.id
+  }
 }
 
 output "ec2_info" {
-    value = aws_instance.ec2_instance
+  value = aws_instance.ec2_instance
 }
 
 output "local_os_username" {
@@ -20,15 +20,15 @@ output "local_os_username" {
 }
 
 output "focer_ipv4" {
-    value = data.external.force_ipv4.result
+  value = data.external.force_ipv4.result
 }
 
 output "ssh_command" {
-    description = "SSH Command you can run to access the image"
-    value = "ssh ec2-user@${aws_instance.ec2_instance.public_dns}"
+  description = "SSH Command you can run to access the image"
+  value       = "ssh ec2-user@${aws_instance.ec2_instance.public_dns}"
 }
 
 output "router_is_ipv6" {
-    description = "Is router defaulting to ipv6"
-    value = local.is_ipv6
+  description = "Is router defaulting to ipv6"
+  value       = local.is_ipv6
 }
